ec99a66b82152ff64417e5968b9dd2c97aa9143d
muhammadnaufalopet <141712648+muhammadnaufalopet@users.noreply.github.com>
2023-10-04 14:52:27 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+function Login(username,pass) {
+    console.log("--------------")
+    console.log("Login")
+    console.log("--------------")
+    console.log("Username:"+(username))
+    console.log("Pass:"+(pass))
+    console.log("--------------")
+}
+Login('opet', '*************')
